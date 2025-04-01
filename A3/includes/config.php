0747e268dcf6de4c3a47b62c7a837c8230453803
Duren Gouda <djgouda26@gmail.com>
2025-04-01 10:56:52 -0300

    Add files via upload

---
+++ A3/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "jokes_db");
+define("DB_USER", "root"); // your DB username, as discussed in class
+define("DB_PSWD", ""); // your DB password, as discussed in class
+
+?>
